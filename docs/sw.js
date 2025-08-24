a64fd864f3bd36ab1ef9fafe43b5cad817c3fbe4
Crispy4222 <crispy4222@users.noreply.github.com>
2025-08-23 22:54:26 -0600

    PWA: manifest + SW + heartbeat 2025-08-24T04:54:26Z

--- docs/sw.js
+++ docs/sw.js
@@ -1,44 +1,6 @@
-const SW_VERSION = '1755943238';
-const CACHE = 'crispy-cache-' + SW_VERSION;
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css?v=1755943238',
-  './app.js?v=1755943238'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open('garage-v1').then(c=>c.addAll(['./','./index.html','./style.css','./heartbeat.json'])));
 });
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
-
-self.addEventListener('fetch', (e) => {
-  const url = new URL(e.request.url);
-  // network-first for HTML, cache-first for static
-  if (url.pathname.endsWith('/') || url.pathname.endsWith('.html')) {
-    e.respondWith(
-      fetch(e.request).then(r => {
-        const clone = r.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return r;
-      }).catch(() => caches.match(e.request))
-    );
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
-        const clone = resp.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return resp;
-      }))
-    );
-  }
-});
-// bump SW 1755943800
